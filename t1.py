4061ae32ac609cf5d554b58e495ded1af24d317d
unknown <mrjmatin8@gmail.com>
2022-02-24 06:27:51 -0800

    project

---
+++ t1.py
@@ -0,0 +1,7 @@
+zel_1 = int(input("zel 1 ra vared knid"))
+zel_2 = int(input("ze 2 ra verd knid"))
+zel_3= int(input("ze 3 ra verd knid"))
+if zel_1 + zel_2 > zel_3 and zel_1 +zel_3 >zel_2  and zel_2 + zel_3 > zel_1:
+    print("true")
+else:
+    print("false")
